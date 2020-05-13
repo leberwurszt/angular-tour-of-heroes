daf0465c954be7c056b9b9d03577c40ed01dd5bb
leberwurszt <dominik.reich@onlinehome.de>
2020-05-13 17:40:58 +0200

    4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,7 @@ export class HeroesComponent implements OnInit
   onSelect(hero: Hero): void 
   {
     this.selectedHero = hero;
-    this.messageService.add('HeroService: Selected hero id=${hero.id}');
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   constructor(private heroService: HeroService, private messageService: MessageService)
