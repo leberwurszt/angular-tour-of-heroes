3d12114ac398a5eb87ddaf71ef2f1c08adcefc65
leberwurszt <dominik.reich@onlinehome.de>
2020-05-13 17:14:50 +0200

    3.2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component
 (
@@ -13,7 +13,7 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit
 {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void 
@@ -21,10 +21,17 @@ export class HeroesComponent implements OnInit
     this.selectedHero = hero;
   }
 
-  constructor()
+  constructor(private heroService: HeroService)
   {}
 
   ngOnInit(): void
-  {}
+  {
+    this.getHeroes();
+  }
+
+  getHeroes(): void
+  {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
 }
